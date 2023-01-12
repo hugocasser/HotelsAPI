71cb17636757afd364306ff30dc97b529d8a2f87
Артём <hugochacasser@gmail.com>
2023-01-12 06:04:05 -0800

    Create class Hotel and add methods : GET, POST, PUT, DELETE.

--- HotelsWebAPI/Program.cs
+++ HotelsWebAPI/Program.cs
@@ -1,6 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var hotels = new List<Hotel>();
 
 app.Run();
+app.MapGet("/hotels", () => hotels);
+app.MapGet("/hotels/{id}", (int id) => hotels.FirstOrDefault(h => h.Id == id));
+app.MapPost("/hotels", (Hotel hotel) => hotels.Add(hotel));
+app.MapPut("/hotels", (Hotel hotel) =>
+{
+    var index = hotels.FindIndex(h => h.Id == hotel.Id);
+    if (index < 0 ) throw new Exception("Not found");
+    hotels[index] = hotel;
+});
+app.MapDelete("/hotels/{id}", (int id) =>
+{
+    var index = hotels.FindIndex(h => h.Id == id);
+    if (index < 0) throw new Exception("Not found");
+    hotels.RemoveAt(index);
+});
+
+public class Hotel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
