f1b7dfd9a86a74fc5e4c7af477163eb031c76aab
Артём <hugochacasser@gmail.com>
2023-01-14 21:32:13 -0900

    add repository for db.

---
+++ HotelsWebAPI/Data/HotelRepository.cs
@@ -0,0 +1,59 @@
+﻿namespace HotelsWebAPI;
+
+public class HotelRepository : IHotelRepository
+{
+    private readonly HotelDb _context;
+    private bool _disposed = false;
+
+    public HotelRepository(HotelDb context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Hotel>> GetHotelsAsync() => 
+        await _context.Hotels.ToListAsync();
+
+    public async Task<Hotel> GetHotelAsync(int hotelId) =>
+        await _context.Hotels.FindAsync(new object[] { hotelId });
+
+    public async Task InsertHotelAsync(Hotel hotel) =>
+        await _context.Hotels.AddAsync(hotel);
+
+    public async Task UpdateHotelAsync(Hotel hotel)
+    {
+        var hotelFromDb = await _context.Hotels.FindAsync(new object[]{ hotel.Id });
+        if (hotelFromDb == null) return;
+        hotelFromDb.Latitude = hotel.Latitude;
+        hotelFromDb.Longitude = hotel.Longitude;
+        hotelFromDb.Name = hotel.Name;
+    }
+
+    public async Task DeleteHotelAsync(int hotelId)
+    {
+        var hotelFromDb = await _context.Hotels.FindAsync(new object[]{ hotelId});
+        if (hotelFromDb == null) return;
+        _context.Hotels.Remove(hotelFromDb);
+    }
+
+    public async Task SaveAsync() =>
+        await _context.SaveChangesAsync();
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+        }
+
+        _disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
--- HotelsWebAPI/Program.cs
+++ HotelsWebAPI/Program.cs
@@ -5,6 +5,7 @@ builder.Services.AddDbContext<HotelDb>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
 });
+builder.Services.AddScoped<IHotelRepository, HotelRepository>();
 
 var app = builder.Build();
 
@@ -15,33 +16,28 @@ if (app.Environment.IsDevelopment())
     db.Database.EnsureCreated();
 }
 
-app.MapGet("/hotels", async (HotelDb db) => await db.Hotels.ToListAsync());
-app.MapGet("/hotels/{id}", async (int id, HotelDb db) => 
-    await db.Hotels.FirstOrDefaultAsync(h => h.Id == id) is Hotel hotel
-    ? Results.Ok(hotel)
-    : Results.NotFound());
-app.MapPost("/hotels", async ([FromBody] Hotel hotel, HotelDb db) =>
+app.MapGet("/hotels", async (IHotelRepository repository) => 
+    Results.Ok(await repository.GetHotelsAsync()));
+app.MapGet("/hotels/{id}", async (int id, IHotelRepository repository) => 
+    await repository.GetHotelAsync(id) is Hotel hotel
+        ? Results.Ok(hotel)
+        : Results.NotFound());
+app.MapPost("/hotels", async ([FromBody] Hotel hotel, IHotelRepository repository) =>
 {
-    db.Hotels.Add(hotel);
-    await db.SaveChangesAsync();
+    await repository.InsertHotelAsync(hotel);
+    await repository.SaveAsync();
     return Results.Created($"/hotels/{hotel.Id}", hotel);
 });
-app.MapPut("/hotels", async ([FromBody] Hotel hotel, HotelDb db) =>
+app.MapPut("/hotels", async ([FromBody] Hotel hotel, IHotelRepository repository) =>
 {
-    var hotelFromDb = await db.Hotels.FindAsync(new object[] {hotel.Id});
-    if (hotelFromDb == null ) return Results.NotFound();
-    hotelFromDb.Latitude = hotel.Latitude;
-    hotelFromDb.Longitude = hotel.Longitude;
-    hotelFromDb.Name = hotel.Name;
-    await db.SaveChangesAsync();
+    await repository.UpdateHotelAsync(hotel);
+    await repository.SaveAsync();
     return Results.NoContent();
 });
-app.MapDelete("/hotels/{id}", async (int id, HotelDb db) =>
+app.MapDelete("/hotels/{id}", async (int id, IHotelRepository repository) =>
 {
-    var hotelFromDb = await db.Hotels.FindAsync(new object[] {id});
-    if (hotelFromDb == null ) return Results.NotFound();
-    db.Hotels.Remove(hotelFromDb);
-    await db.SaveChangesAsync();
+    await repository.DeleteHotelAsync(id);
+    await repository.SaveAsync();
     return Results.NoContent();
 });
 
